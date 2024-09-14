df19602fde328f7bdf81c5fca210c79e86d7e1b9
bradgerb <77023448+bradgerb@users.noreply.github.com>
2024-09-14 17:23:46 -0400

    Set up buttons 1-9

--- javascript.js
+++ javascript.js
@@ -22,6 +22,38 @@ const setOne = function(){
     changeDisplay(1);
 }
 
+const setTwo = function(){
+    changeDisplay(2);
+}
+
+const setThree = function(){
+    changeDisplay(3);
+}
+
+const setFour = function(){
+    changeDisplay(4);
+}
+
+const setFive = function(){
+    changeDisplay(5);
+}
+
+const setSix = function(){
+    changeDisplay(6);
+}
+
+const setSeven = function(){
+    changeDisplay(7);
+}
+
+const setEight = function(){
+    changeDisplay(8);
+}
+
+const setNine = function(){
+    changeDisplay(9);
+}
+
 const changeDisplay = function(){
     const display = document.querySelector(".display");
     display.style.color = "blue";
@@ -29,3 +61,19 @@ const changeDisplay = function(){
 
 const oneButton = document.querySelector(".one");
 oneButton.addEventListener("click", setOne);
+const twoButton = document.querySelector(".two");
+twoButton.addEventListener("click", setTwo);
+const threeButton = document.querySelector(".three");
+threeButton.addEventListener("click", setThree);
+const fourButton = document.querySelector(".four");
+fourButton.addEventListener("click", setFour);
+const fiveButton = document.querySelector(".five");
+fiveButton.addEventListener("click", setFive);
+const sixButton = document.querySelector(".six");
+sixButton.addEventListener("click", setSix);
+const sevenButton = document.querySelector(".seven");
+sevenButton.addEventListener("click", setSeven);
+const eightButton = document.querySelector(".eight");
+eightButton.addEventListener("click", setEight);
+const nineButton = document.querySelector(".nine");
+nineButton.addEventListener("click", setNine);
