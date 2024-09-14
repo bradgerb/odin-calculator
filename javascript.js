b69fceb8ef41afa517b423e854f5450bc610c0e3
bradgerb <77023448+bradgerb@users.noreply.github.com>
2024-09-14 17:13:27 -0400

    Begin writing display change functions

---
+++ javascript.js
@@ -0,0 +1,27 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = 0;
+
+const sum = function(a, b){
+    return a+b
+}
+
+const subtract = function(a, b){
+    return a-b
+}
+
+const product = function(a, b){
+    return a*b
+}
+
+const divide = function(a, b){
+    return a/b
+}
+
+const changeDisplay = function(){
+    const display = document.querySelector(".display");
+    display.style.color = "blue";
+}
+
+const oneButton = document.querySelector(".one");
+oneButton.addEventListener("click", changeDisplay);
